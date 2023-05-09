8e73562c06da5c1a00bd1ad2d2180ab1fc6b62c3
Andrew Barba <barba@hey.com>
2023-05-09 12:01:34 -0400

    Vercel

--- lambda.js
+++
@@ -1,9 +0,0 @@
-const awsServerlessExpress = require('aws-serverless-express')
-const express = require('express')
-const serveStatic = require('serve-static')
-const app = express()
-const server = awsServerlessExpress.createServer(app)
-
-app.use(serveStatic('./src'))
-
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
