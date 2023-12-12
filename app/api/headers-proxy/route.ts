c71d8bb522c4eb6c0fa5f4ddc7d9748cadde762c
Andrew Barba <barba@hey.com>
2023-12-12 15:43:34 -0500

    Cleanup

--- app/api/headers-proxy/route.ts
+++ app/api/headers-proxy/route.ts
@@ -2,7 +2,6 @@ export const dynamic = 'force-dynamic'
 
 export async function GET(req: Request) { 
   const url = new URL('/api/headers', `https://${req.headers.get('host')}`)
-  console.log('fetching url', url.toString())
   const res = await fetch(url, {
     headers: {
       'cookie': '_vercel_debug_request_challenges=1'
