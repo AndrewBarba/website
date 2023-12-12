bd064794025cb5de0602427bb2b59478dd835b80
Andrew Barba <barba@hey.com>
2023-12-12 13:31:45 -0500

    Send cookie

--- app/api/edge/headers-proxy/route.ts
+++ app/api/edge/headers-proxy/route.ts
@@ -2,6 +2,10 @@ export const runtime = 'edge'
 export const dynamic = 'force-dynamic'
 
 export async function GET(req: Request) { 
-  const url = new URL('/api/edge/headers', `https://${process.env.VERCEL_URL}`)
-  return fetch(url, { cache: 'no-cache' })
+  const url = new URL('/api/headers', `https://${req.headers.get('host')}`)
+  return fetch(url, {
+    headers: {
+      'cookie': '_vercel_debug_request_challenges=1'
+    }
+  })
 }
--- app/api/headers-proxy/route.ts
+++ app/api/headers-proxy/route.ts
@@ -1,6 +1,10 @@
 export const dynamic = 'force-dynamic'
 
 export async function GET(req: Request) { 
-  const url = new URL('/api/headers', `https://${process.env.VERCEL_URL}`)
-  return fetch(url, { cache: 'no-cache' })
+  const url = new URL('/api/headers', `https://${req.headers.get('host')}`)
+  return fetch(url, {
+    headers: {
+      'cookie': '_vercel_debug_request_challenges=1'
+    }
+  })
 }
